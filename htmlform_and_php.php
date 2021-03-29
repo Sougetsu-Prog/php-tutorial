8a64b87c995e06700667df38999492423c92541e
Sougetsu <[kazuxtopazius@gmail.com]>
2021-03-29 19:37:07 +0000

    Fix: 細かな修正

--- htmlform_and_php.php
+++ htmlform_and_php.php
@@ -4,15 +4,16 @@ if ($_POST["name"] || $_POST["age"]) {
         die("無効な名前です。");
     }
     
-    echo "ようこそ". $_POST['name']. "<br />";
-    echo "You are ". $_POST['age']. " years old.<br />";
+    echo "ようこそ ". $_POST['name']. " さん。<br />";
+    echo "あなたは ". $_POST['age']. " 歳です。<br />";
 
     exit();
 }
 ?>
 <html>
   <body>
-    <form action="<?php $_SERVER["SCRIPT_NAME"] ?>" method="POST">
+    <!-- $_SERVER['SCRIPT_NAME']現在のスクリプトのパス。 スクリプト自身のページを指定する。 -->
+    <form action="<?php $_SERVER['SCRIPT_NAME'] ?>" method="POST">
       Name: <input type="text" name="name" />
       Age: <input type="text" name="age" />
       <input type="submit" />
